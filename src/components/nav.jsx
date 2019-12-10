e6be161e43715800b5aa33bb115bd7aba09e098a
Nabil Mehtab <nabilk@uni-paderborn.de>
2019-12-10 14:38:35 +0100

    Top/Nav component added

---
+++ src/components/nav.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { directive } from "@babel/types";
+class Nav extends Component {
+  state = {
+    giturl: "https://github.com/akshay05091992/Entity-Description-Generation-UI"
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col">
+            <i
+              style={{ fontSize: "48px", color: "blue" }}
+              className="fa fa-info-circle float-left"
+            ></i>
+          </div>
+          <div className="col text-center">
+            <h1>Welcome!</h1>
+          </div>
+          <div className="col">
+            <a href={this.state.giturl} target="_blank">
+              {" "}
+              <i
+                style={{ fontSize: "48px", color: "black" }}
+                className="fab fa-github float-right"
+              ></i>
+            </a>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import Nav from "./components/nav";
-import Search from "./components/search";
+
+ReactDOM.render(<Nav />, document.getElementById("nav"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
