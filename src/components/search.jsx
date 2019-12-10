37a92d203d62b9baca07e1d14958aaae5f8ae2ee
Nabil Mehtab <nabilk@uni-paderborn.de>
2019-12-10 14:45:44 +0100

    Search Component Added

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,1 +1,21 @@
+import React, { Component } from "react";
+class Search extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <p>
+          <i>
+            {" "}
+            <input type="text" className="input" placeholder="Class" />
+          </i>
+          <i>
+            <input type="text" className="input" placeholder="Search..." />{" "}
+          </i>
+        </p>
+      </React.Fragment>
+    );
+  }
+}
 
+export default Search;
